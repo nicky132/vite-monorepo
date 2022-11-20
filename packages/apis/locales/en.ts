e3e0a8b1c8683884aa6e4d93da26509cb617fcba
nicky132 <hhq87776153@hotmail.com>
2022-11-21 00:54:57 +0800

    monorepo多包架构

---
+++ packages/apis/locales/en.ts
@@ -0,0 +1,11 @@
+export default {
+  '401': 'Request is unauthorized',
+  '403': 'Request is forbidden',
+  '404': 'Resource not found',
+  '405': 'Method not allowed',
+  '500': 'Service not available',
+  BadCredentials: 'Login failed',
+  UserAlreadyJoined: 'User already Joined',
+  UserAlreadyExisted: 'User already existed',
+  '100404': 'User not found',
+}
